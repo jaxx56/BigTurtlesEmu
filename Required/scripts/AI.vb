6ad66ebd148954696bc04409caae17b04044c79e
BillytheOrc <turtleswin1337@gmail.com>
2013-04-29 15:39:52 -0700

    Added Testing AI, may need to be removed.

--- Required/scripts/AI.vb
+++ Required/scripts/AI.vb
@@ -27,6 +27,25 @@ Namespace Scripts
 
 	End Class
 
+	Public Class TestAI
+	Inherits TBaseAI
+	
+	Public Craeture as CreatureObject
+	Public Sub New(ByRef ParentCreature as CreatureObject)
+		Creature = ParentCreature
+	End Sub
+	Public Overrides Sub Dispose()
+	End Sub
+	
+	Public Overrides Sub OnAttack(ByRef Attacker As BaseUnit)
+		If TypeOf Attacker Is CharacterObject Then
+		Creature.SendChatMessage("Am leet test mob for Brad and Jak.", ChatMsg.CHAT_MSG_MONSTER_SAY, LANGUAGES.LANG_UNIVERSAL)
+		
+	End If
+End Sub
+
+End Class	
+
 
 
 
