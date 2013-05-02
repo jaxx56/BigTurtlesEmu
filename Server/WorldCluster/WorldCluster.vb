c0f93481e35d40a7305054ad63e6e9e3cc59ec45
BillytheOrc <turtleswin1337@gmail.com>
2013-05-02 15:08:29 -0700

    Update to prevent a DMCA attack.

--- Server/WorldCluster/WorldCluster.vb
+++ Server/WorldCluster/WorldCluster.vb
@@ -1,6 +1,6 @@
 ' 
-' Copyright (C) 2008 BigTurtlesEmu <https://bigturtlesemu.weebly.com>
-'
+' Copyright (C) 2012-2013 BigTurtlesEmu <https://bigturtlesemu.weebly.com>
+' Copyright (C) 2008-2012 SpuriousEmu
 ' This program is free software; you can redistribute it and/or modify
 ' it under the terms of the GNU General Public License as published by
 ' the Free Software Foundation; either version 2 of the License, or
